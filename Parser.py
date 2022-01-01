159fcd3df43b17dbb733cabd6d5010d983b8a4b5
Cem Üstün <ustuncem7@gmail.com>
2022-01-01 23:01:33 +0300

    Parser Done. Lagrange 0.1

--- Parser.py
+++ Parser.py
@@ -1,17 +1,53 @@
 import pandas as pd
+import numpy as np
+from pandas.core.frame import DataFrame
 
 class Parser:
-    """[summary]
+    """ Initializing and Cleaning Data
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, filename: str) -> None:
+        self.raw_data = pd.read_excel(filename)
+        self.filename = filename
+        self.clean_data = self.__parse(columns_to_drop=["Difference"])
 
-    def parse(self):
-        raw_data = pd.read_excel("./assets/AVAX.xlsx")
-        print(raw_data)
+    def debug(self) -> None:
+        """Debugger 
+        """
+        print("Raw Data: \n", self.raw_data)
+        print("Raw Data Info:\n")
+        self.raw_data.info()
+        print("Raw Data / Types: \n", self.raw_data.dtypes)
+
+        print("Clean Data: \n", self.clean_data)
+        print("Clean Data Info:\n")
+        self.clean_data.info()
+        print("Clean Data / Types: \n", self.clean_data.dtypes)
+
+    def __parse(self, columns_to_drop = []) -> DataFrame:
+        """Main method to parse incoming Excel.
+
+        Args:
+            columns_to_drop (list, optional): [Columns to drop]. Defaults to [].
+
+        Returns:
+            DataFrame: [Pandas DataFrame object]
+        """
+
+        self.raw_data = self.raw_data.set_index("Day")
+
+        if len(columns_to_drop) > 0:
+            self.raw_data.drop(columns = columns_to_drop, inplace=True)
+
         
+        self.raw_data.fillna(0)
+
+        #Sort Data (ASC)
+        self.raw_data = self.raw_data.sort_index(ascending=True)
+        
+        # Cleaned Data
+        return self.raw_data
 
 
-x = Parser()
-x.parse()
+data = Parser("./assets/AVAX.xlsx")
+print(data.clean_data)
