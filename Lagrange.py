fff6a70a96c0ad8b8e21db498286900b6e1d0c35
Cem Üstün <ustuncem7@gmail.com>
2022-01-01 23:01:12 +0300

    Parser Done. Lagrange 0.1

---
+++ Lagrange.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Lagrange:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def polynomial():
+        pass
+
+    @staticmethod
+    def basis(parameter: int, iteration: int, ) -> int:
+        pass
